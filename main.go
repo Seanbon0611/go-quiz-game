797d23492de19919184464888a9c0ed680af1af2
Seanbon0611 <sean.dever@datadoghq.com>
2021-11-17 18:14:09 -0800

    added struct to hold questions and answers, loop through and print each iteration of the questions and answers from the csv file was successful

--- main.go
+++ main.go
@@ -1,16 +1,31 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
 	"os"
 )
 
+type questionsAndAnswers struct {
+	Question string
+	Answer   string
+}
+
 func main() {
-	file, err := os.ReadFile("problems.csv")
+	file, err := os.Open("./problems.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(string(file))
+	defer file.Close()
+	reader, _ := csv.NewReader(file).ReadAll()
+	fmt.Println(reader)
 
+	for _, line := range reader {
+		qAndA := questionsAndAnswers{
+			Question: line[0],
+			Answer:   line[1],
+		}
+		fmt.Println(qAndA.Question + " " + qAndA.Answer)
+	}
 }
