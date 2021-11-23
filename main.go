75926ed23660bc71f2e70e612c0f2a9e05a8dbad
Seanbon0611 <sean.dever@datadoghq.com>
2021-11-22 16:38:45 -0800

    added feature to display each problem individually, have the user submit an answer and tally the correct answers to display at the end

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -13,21 +14,49 @@ type questionsAndAnswers struct {
 }
 
 func main() {
+	csvfile := flag.String("csv", "problems.csv", "A quiz game CLI application that takes in csv file in the format of `question,answer`")
+
+	flag.Parse()
+
 	//opens the csv file to be able to read
-	file, err := os.Open("./problems.csv")
+	file, err := os.Open(*csvfile)
 	if err != nil {
-		log.Fatal(err)
+		returnError(err)
 	}
 	//what gets opened must be closed, don't forget!
 	defer file.Close()
-	//turns contents of the csv file into somethijng readable rather than just a pointer to the csv file contents
+	//turns contents of the csv file into something readable rather than just a pointer to the csv file contents
 	reader, _ := csv.NewReader(file).ReadAll()
-	//loop through the reader and create new instances of the questionsAndAnswers struct and print the results
-	for _, line := range reader {
-		qAndA := questionsAndAnswers{
+	problems := parseQAndA(reader)
+	//keeps track of how many answers the user gets correct
+	correctAnswers := 0
+	//loop through each problem within the slice of problems, give the question and have the customer submit the answer
+	for i, problem := range problems {
+		fmt.Printf("Problem # %d: %s = \n", i+1, problem.Question)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == problem.Answer {
+			correctAnswers += 1
+		}
+	}
+	fmt.Printf("your score is %d/%d\n", correctAnswers, len(problems))
+}
+
+//function to parse the questions and answers into a slice that will hold each question and answer struct
+func parseQAndA(lines [][]string) []questionsAndAnswers {
+	//create a slice that will create new instances of the questionsAndAnswers struct that will be the length of the lines
+	parsedProblems := make([]questionsAndAnswers, len(lines))
+	for i, line := range lines {
+		parsedProblems[i] = questionsAndAnswers{
 			Question: line[0],
 			Answer:   line[1],
 		}
-		fmt.Println(qAndA.Question + " " + qAndA.Answer)
 	}
+	return parsedProblems
+}
+
+//If there is an error, log it and return an exist status of 1
+func returnError(err error) {
+	log.Fatal(err)
+	os.Exit(1)
 }
